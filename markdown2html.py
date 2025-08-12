b52f38d9df34844a310d042b349dec0bb829b42f
vlldnt <vieilledent.adrien@gmail.com>
2025-08-12 10:25:02 +0200

    feat: star cript / Handling errors

--- markdown2html.py
+++ markdown2html.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+'''Markdown to HTML converter script'''
+
+
+import sys
+import os
+
+
+def main():
+    if len(sys.argv) < 3:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+        sys.exit(1)
+
+    input_md = sys.argv[1]
+    output_html = sys.argv[2]
+
+    if not os.path.isfile(input_md):
+        sys.stderr.write(f"Missing {input_md}\n")
+        sys.exit(1)
+
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
